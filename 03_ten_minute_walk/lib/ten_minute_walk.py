ca3e0797378e80c79dba73f3fa7bf435717248de
Evan Thomas <evanthomas0292@gmail.com>
2024-02-01 18:33:06 +0000

    Refactor for readability

--- 03_ten_minute_walk/lib/ten_minute_walk.py
+++ 03_ten_minute_walk/lib/ten_minute_walk.py
@@ -1,12 +1,11 @@
 from collections import Counter
 
 def ten_minute_walk(directions: [str]) -> bool:
-    if len(directions) != 10:
-        return False
-
     counter = Counter(directions)
 
-    if counter['n'] == counter['s'] and counter['w'] == counter['e']:
-        return True
+    return all([
+        len(directions) == 10,
+        counter['n'] == counter['s'],
+        counter['w'] == counter['e'],
+    ])
 
-    return False
